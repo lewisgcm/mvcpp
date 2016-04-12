5f5f2800057792a0fc2071f281d59828a094420a
Lewis Maitland <lewisgcm@gmail.com>
2016-04-12 22:22:38 +0100

    Adding fast example for new string splitter

--- benchmarking/request/source.cpp
+++ benchmarking/request/source.cpp
@@ -9,10 +9,9 @@ int main() {
 
     for(int i=0; i< ITERATIONS; i++) {
         istringstream input(
-            "POST /api/test/hello?some=parameters&to=test&parsing=ability&empty&ones=aswell HTTP/1.1\r\n"
+            "POST /api/test/hello HTTP/1.1\r\n"
             "Header: test\r\n"
             "Another: Header\r\n"
-            "Header1: This is a very very very very long header that should test the ability to parse the longer strings\r\n"
             "cookie: name=lewis\r\n"
             "\r\n"
             "And now for the body blah blah this is the body of the request"
